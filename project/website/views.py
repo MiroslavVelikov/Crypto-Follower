e808a157f239e29792148b6c2ce4c1279b8fa21e
MiroslavVelikov <mir.velikov@gmail.com>
2022-02-26 23:55:54 +0200

    Fixing alerts

--- project/website/views.py
+++ project/website/views.py
@@ -2,7 +2,7 @@ from . import db
 from .models import User
 from .ScrapingInfo import MarketInfo, SliderInfo, CurrencyDetails
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask import Blueprint, request, render_template, redirect, url_for
+from flask import Blueprint, request, render_template, redirect, url_for, flash
 
 views = Blueprint('views', __name__)
 
@@ -38,6 +38,7 @@ def Login():
             print("Logged in")
         else:
             print("Invalid email or password")
+            
 def Register():
     email = request.form.get("email")
     username = request.form.get("username")
@@ -45,21 +46,20 @@ def Register():
     repassword = request.form.get("re-password")
     
     if len(email) < 4:
-        pass
+        flash("Email must be greater than 4 charecters!")
     elif len(username) < 2:
-        pass
+        flash("Username must be greater than 2 charecters!")
     elif len(password) < 7:
-        pass
+        flash("Password must be greater than 7 charecters!")
     elif password != repassword:
-        pass
+        flash("Passwords do not match!")
     else:
-        pass
-
-    new_user = User(email=email, username=username, password=generate_password_hash(password, method="sha256"))
-    db.session.add(new_user)
-    db.session.commit()
-    print("Registered succesfully")
+        new_user = User(email=email, username=username, password=generate_password_hash(password, method="sha256"))
+        db.session.add(new_user)
+        db.session.commit()
+        flash("Succesfully registered")
 
     return redirect(url_for("views.home"))
+
 def CheckRequest():
     return request.form.get("request")
